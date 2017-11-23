27ac969c62059e8d0e24f76b7a4772a5278c2873
tstewart137 <33452705+tstewart137@users.noreply.github.com>
2017-11-23 07:03:35 -0500

    Delete creds.py

--- creds.py
+++
@@ -1,6 +0,0 @@
-{
-'consumer_key' : "eKBZXeitHuSQfKAVoS3wN4hfb",
-'consumer_secret' : "JQRkCUVC9zZ4U2pzF3c8z2cet1Qn4SMJQwQw4148NT6eGvKN5v",
-'access_token' : "899783081422356485-aj9hTgwEG33mbYzBgPAZA9PoHYRhJ8I",
-'access_token_secret' : "Q0rhx6gVUMzmqWYtuUOS0aDyqeUIEU6eurxQyyjmKTmS5"
-}
